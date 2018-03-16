14209f053ab7638c7e5b9d15fe8af24236809454
Alastair Droop <alastair.droop@gmail.com>
2018-03-16 12:46:42 +0000

    Simplify option formatting.

--- sections.py
+++ sections.py
@@ -90,33 +90,16 @@ class Log(object):
     name = property(getName, setName, "The log file name")
 
 class Option(object):
-    option_re = re.compile('^\s*[-]?(?P<switch>\w+)\s*(?:(?P<argument>\w+)(?:=(?P<value>\S+))?)?\s*$')
     @classmethod
     def fromString(cls, string):
-        match = cls.option_re.match(string)
-        if match is None: raise ValueError('invalid option string: "{}"'.format(string))
-        return Option(match.group('switch'), match.group('argument'), match.group('value'))
-    def __init__(self, switch, argument=None, value=None):
-        self.switch = switch
-        self.argument = argument
-        self.value = value
-    def setSwitch(self, switch): self._switch = switch
-    def setArgument(self, argument): self._argument = argument
+        if string.startswith('-'):
+            string = string.lstrip('-')
+        return Option(string)
+    def __init__(self, string):
+        self.value = string
     def setValue(self, value): self._value = value
-    def getSwitch(self): return self._switch
-    def getArgument(self): return self._argument
-    def getFullSwitch(self):
-        if self.argument == None: return self.switch
-        return '{} {}'.format(self.switch, self.argument)
     def getValue(self): return self._value
-    def __str__(self, prefix='-'):
-        output = '{}{}'.format(prefix, self.switch)
-        if self.argument != None: output = '{} {}'.format(output, self.argument)
-        if self.value != None: output = '{}={}'.format(output, self.value)
-        return output
-    switch = property(getSwitch, setSwitch)
-    fullswitch = property(getFullSwitch, None)
-    argument = property(getArgument, setArgument)
+    def __str__(self, prefix='-'): return '{}{}'.format(prefix, self.value)
     value = property(getValue, setValue)
 
 class OptionList(object):
